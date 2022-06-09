58d372eee2cf4bd75cd38c2ddd36c87fb4397e19
doglez <danilo.j.gonzalez@gmail.com>
2022-06-08 19:58:09 -0600

    Dashboard table and filters

--- client/src/pages/Home/Dashboard/Dashboard.jsx
+++ client/src/pages/Home/Dashboard/Dashboard.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import SummaryTable from "./SummaryTable.jsx";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return (
+        <div className="container">
+            <div className="d-flex justify-content-between">
+                <select
+                    className="form-select mb-3 border-pink"
+                    aria-label="Default select example"
+                >
+                    <option defaultValue>Year</option>
+                    <option value={1}>One</option>
+                    <option value={2}>Two</option>
+                    <option value={3}>Three</option>
+                </select>
+
+                <select
+                    className="form-select mb-3 mx-5 month-select border-orange"
+                    aria-label="Default select example"
+                >
+                    <option defaultValue>Month</option>
+                    <option value={1}>One</option>
+                    <option value={2}>Two</option>
+                    <option value={3}>Three</option>
+                </select>
+
+                <select
+                    className="form-select mb-3 department-select border-teal"
+                    aria-label="Default select example"
+                >
+                    <option defaultValue>Department</option>
+                    <option value={1}>One</option>
+                    <option value={2}>Two</option>
+                    <option value={3}>Three</option>
+                </select>
+            </div>
+            <SummaryTable />
+        </div>
+    );
 };
 
 export default Dashboard;
