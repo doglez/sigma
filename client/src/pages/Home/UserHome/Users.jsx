a78dc2bdba095ed3162ba41ddc71ca3fa4d25ae0
doglez <danilo.j.gonzalez@gmail.com>
2022-06-28 17:58:46 -0600

    list of users

--- client/src/pages/Home/UserHome/Users.jsx
+++ client/src/pages/Home/UserHome/Users.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import NoUserPhoto from "../../../assets/images/NoUserPhoto.png";
+import { getUserCrt } from "../../../redux/reducers/usersSlice.js";
 
 const Users = () => {
+    const dispatch = useDispatch();
+
+    const handlerReload = () => {
+        dispatch(getUserCrt());
+    };
+
+    useEffect(() => {
+        window.addEventListener("beforeunload", handlerReload());
+    }, []);
+
+    const users = useSelector((state) => state.usersReducer.data);
+
     return (
         <div className="container">
             <h1 className="text-deep-saffron py-4 text-center">
@@ -11,56 +26,67 @@ const Users = () => {
             <Link role="button" className="btn btn-primary mb-2" to="new">
                 New User
             </Link>
-
-            <div className="scrollable-horizontal">
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Collaborator #</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Last Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Department</th>
-                            <th scope="col">Role</th>
-                            <th scope="col">Picture</th>
-                            <th scope="col">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>25</td>
-                            <td>Danilo</td>
-                            <td>Gonzalez</td>
-                            <td>danilo.gonzalez@example.com</td>
-                            <td>Data Center</td>
-                            <td>Engineer</td>
-                            <td>
-                                <img
-                                    src={NoUserPhoto}
-                                    className="rounded-circle photo-user"
-                                    alt="..."
-                                />
-                            </td>
-                            <td>
-                                <Link
-                                    role="button"
-                                    className="btn btn-warning mb-md-2 mb-lg-0 me-lg-2 me-md-0 button-action"
-                                    to="edit/1"
-                                >
-                                    Update
-                                </Link>
-                                <Link
-                                    role="button"
-                                    className="btn btn-danger button-action"
-                                    to="delete/1"
-                                >
-                                    Delete
-                                </Link>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            {!users ? (
+                <div>Your Data is loading...</div>
+            ) : (
+                <div className="scrollable-horizontal scrollable-horizontal-allways">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Collaborator #</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Last Name</th>
+                                <th scope="col">Email</th>
+                                {/* <th scope="col">Department</th> */}
+                                <th scope="col">Picture</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr key={user._id}>
+                                    <td>{user.collaboratorNumber}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.email}</td>
+                                    {/* <td>{user.department}</td> */}
+                                    <td>
+                                        {!user.photo ? (
+                                            <img
+                                                src={NoUserPhoto}
+                                                className="rounded-circle photo-user"
+                                                alt="..."
+                                            />
+                                        ) : (
+                                            <img
+                                                src={`${process.env.REACT_APP_FILE_URL_SERVER}/${user.photo}`}
+                                                className="rounded-circle photo-user"
+                                                alt="..."
+                                            />
+                                        )}
+                                    </td>
+                                    <td>
+                                        <Link
+                                            role="button"
+                                            className="btn btn-warning mb-md-2 mb-lg-0 me-lg-2 me-md-0 button-action"
+                                            to={`edit/${user._id}`}
+                                        >
+                                            Update
+                                        </Link>
+                                        <Link
+                                            role="button"
+                                            className="btn btn-danger button-action"
+                                            to={`delete/${user._id}`}
+                                        >
+                                            Delete
+                                        </Link>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
     );
 };
